ef2eba0334a23add17752a1c2b0f9f37a9eb2f85
fgonagu <melly.fran@gmail.com>
2025-11-22 15:29:00 +0100

    Finalización vista y comienzo de la lógica (variables del juego)

--- 3_en_raya/app/src/main/java/com/example/tresenraya/MainActivity.java
+++ 3_en_raya/app/src/main/java/com/example/tresenraya/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tresenraya;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables Vista
+    private ImageButton[] casillas; //Array para manejar los 9 botones
+    private Button btnStart;
+    private RadioButton rbFacil, rbDificil;
+    private RadioButton radioGroup;
+    private TextView tvTitulo, tvTurno;
+
+    //Variables Lógica del juego
+    private boolean juegoIniciado = false; //Controla si el juego está activo
+    private int[] tablero = new int[9];
+    private int turno = 1; //1 -> Turno del usuario (empieza el usuario por defecto)
+    private int nivelDificultad = 0; // 0 -> no seleccionado, 1 -> Fácil, 2 -> Difícil
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Vinculación de vistas
+        btnStart = findViewById(R.id.btn_Start);
+
+
+
+
+
+
+
+
     }
 }
